999dcf9d33bfceea45a9fd6c7789de84740076d7
Ferran Gimeno <fgimeno@intercomempresas.com>
2019-04-04 18:34:41 +0200

    simple endpoint test

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,20 +4,33 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController extends AbstractController
 {
     /**
-     * @Route("/")
+     * Simple test of twig render template
      *
+     * @Route("/", name="test_page")
      * @return Response
      */
-    public function initTest() {
+    public function initTest()
+    {
 
         return $this->render('test/show.html.twig', [
            'title'  =>  "this is a Test",
            'text'   =>  "lorem ipsum dolor sita amet kalsjdkla sdals dajdl ad asd"
         ]);
+    }
+
+    /**
+     *  Return an api json repsonse
+     *
+     * @Route("/api-test", name="api-test")
+     */
+    public function apiEndpointTest()
+    {
+        return new JsonResponse(['lorem' => 'ipsum']);
     }
 }
